e8d043432da8d4e542276365c23957a313a3d32c
maheshkmp <maheshpererakm@gmail.com>
2024-06-11 19:40:16 +0530

    admin settings

--- app_users/admin.py
+++ app_users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from app_users.models import user_profile
 
 # Register your models here.
+
+admin.site.register(user_profile)
--- app_users/models.py
+++ app_users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+
 def path_and_rename( instance, filename):
     upload_to = 'Images/'
     ext = filename.split('.')[-1]
