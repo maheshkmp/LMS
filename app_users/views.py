620a4030734ab454bf68b8159c46804e49e05603
Mahesh Perera <165042259+maheshkmp@users.noreply.github.com>
2024-07-03 16:02:56 +0530

    update

--- app_users/views.py
+++ app_users/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from app_users.forms import UserForm, UserProfileInfoForm
-
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
-    return HttpResponse('This is home page')
+    return render(request, 'home.html')
 
 def register(request):
 
@@ -25,3 +27,41 @@ def register(request):
             profile.save()
 
             registered = True
+
+        else:
+            print(user_form.errors, profile_form.errors)
+
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileInfoForm()
+
+    return render(request, 'app_users/registration.html', 
+                  {'registered': registered, 
+                   'user_form': user_form, 
+                   'profile_form': profile_form})
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("ACCOUNT IS DEACTIVATED")
+            
+        else:
+            return HttpResponse("Please use correct id and password")
+        
+    
+    else:
+        return render(request, 'app_users/login.html')
+    
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
